bb55ad4adf0f6268199e28fd2d551b2523893abf
CHAYANON_cs017 <61398454+CyoiydgTv2542@users.noreply.github.com>
2020-10-02 16:08:00 +0700

    update calendar and form ebooking

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,114 +6,176 @@
 <div class="row">
 <div class="col-lg-12 margin-tb">
 <div class="card">
+
+    <div class="card card-info">
+        <div class="card-header">
+          <label class="card-title">รายละเอียดการจองห้องประชุม</label>
+        </div>
+        </div>
+<div class="card-body">
+<div class="row">  
+    <div class="card card-info">      
 <div class="card-header">
-    
-<div class="pull-left">
-<center><h2>แบบฟอร์มการจองห้องประชุมออนไลน์</h2></center>
-</div>
-<!-- radio button เลือกคำนำหน้าชื่อ -->
-<div class="custom-control custom-radio custom-control-inline">
-<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
-<label class="custom-control-label" for="customRadioInline1">นาย</label>
-</div>
-<div class="custom-control custom-radio custom-control-inline">
-<input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
-<label class="custom-control-label" for="customRadioInline2">นาง</label>
-</div>
-<div class="custom-control custom-radio custom-control-inline">
-<input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
-<label class="custom-control-label" for="customRadioInline2">นางสาว</label>
-</div>
-
-<!-- text กรอกข้อมูล -->
-<form>
-<div class="form-row">
-<div class="col-md-3">
-<label for="inputEmail4">ชื่อ</label>
-<input type="text" class="form-control" placeholder="ระบุชื่อ">
-</div>
-<div class="col-md-3">
-<label for="inputEmail4">นามสกุล</label>
-<input type="text" class="form-control" placeholder="ระบุนามสกุล">
-</div>
-<!--state เลือกประเภท -->
-<div class="col-md-2">
-<label for="inputState">ตำแหน่ง</label>
-<select id="inputState" class="form-control">
-<option selected>กรุณาเลือก</option>
-<option>...</option>
-</select>
-</div>
-</div>
+    <h3 class="card-title">ผู้จอง</h3>
+    </div>
+    </div>
 
 
 
-<div class="form-row">
-<div class="form-group col-md-3">
-<label for="inputEmail4">Email</label>
-<input type="email" class="form-control" id="inputEmail4">
-</div>
-<div class="form-group col-md-3">
-<label for="inputEmail4">เบอร์โทรศัพทร์</label>
-<input type="email" class="form-control" id="inputEmail4">
-</div>
-</div>
-
-<div class="col-md-2">
-<label for="inputState">ห้องประชุม</label>
-<select id="inputState" class="form-control">
-<option selected>กรุณาเลือก</option>
-<option>...</option>
-</select>
-</div>
-
-<div class="form-group col-md-2">
-<label for="inputZip">จำนวนผู้เข้ารวมงาน</label>
-<input type="text" class="form-control" id="inputZip">
-</div>
-
-<div class="form-row">
-<div class="form-group col-md-6">
-<label for="inputCity">City</label>
-<input type="text" class="form-control" id="inputCity">
-</div>
-
-</div>
-
-<label for="inputState">กรุณาเลือกรูปแบบห้องประชุม</label>
-<div class="form-check">
-<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-<label class="form-check-label" for="exampleRadios1">
-รูปแบบที่ 1
-</label>
-</div> 
-<div class="form-check">
-<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-<label class="form-check-label" for="exampleRadios1">
-รูปแบบที่ 2
-</label>
-</div> 
-
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+        <label> หน่วยงาน  </label>
+        <input type="text" name="coderoom" class="form-control" placeholder="หน่วยงาน">
+        </div>
+        </div>
+<div class="col-xs-12 col-sm-12 col-md-12">
 <div class="form-group">
-<label for="inputState">กรุณาเลือกอุปกรณ์</label>
-<div class="form-check">
-<input class="form-check-input" type="checkbox" id="gridCheck">
-<label class="form-check-label" for="gridCheck">
-อุปกรณ์ 1<br>
-<input class="form-check-input" type="checkbox" id="gridCheck">
-<label class="form-check-label" for="gridCheck">
-อุปกรณ์ 2<br>
-<input class="form-check-input" type="checkbox" id="gridCheck">
-<label class="form-check-label" for="gridCheck">
-อุปกรณ์ 3<br>
-</label>
+<label> ชื่อ  </label>
+<input type="text" name="coderoom" class="form-control" placeholder="ชื่อ">
 </div>
 </div>
-<button type="submit" class="btn btn-primary">ตกลง</button>
-</form>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label> นามสกุล  </label>
+<input type="text" name="coderoom" class="form-control" placeholder="นามสกุล">
+</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label> ตำแหน่ง  </label>
+<input type="text" name="coderoom" class="form-control" placeholder="ตำแหน่ง">
+</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label> email  </label>
+<input type="text" name="email" class="form-control" placeholder="email">
+</div>
+</div>
+<!-- phone mask -->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label>เบอร์โทรศัพท์</label>
+<div class="input-group">
+<div class="input-group-prepend">
+<span class="input-group-text"><i class="fas fa-phone"></i></span>
+</div>
+<input type="text" class="form-control"
+data-inputmask="'mask': ['999-999-9999 ']" data-mask>
+</div>
+</div>
+</div> 
+<!-- วันจอง -->
+<div class="card card-info">
+<div class="card-header">
+<label class="card-title">รายละเอียดและวันที่จอง</label>
+</div>
+</div>
+<!-- select -->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label>ห้องประชุม</label>
+<select class="form-control">
+<option> เจ้าเสวย</option>
+<option> กาละเกด</option>
+<option> 3</option>
+<option> 4</option>
+<option> 5</option>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label for="exampleInputPassword1">จำนวนผู้เข้าร่วม</label>
+<input type="num" class="form-control" id="exampleInputPassword1" placeholder="จำนวนผู้เข้าร่วม">
+</div>
+</div>
+<!-- Date and time range -->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label>เวลาในการจอง</label>
+  <div class="input-group">
+<div class="input-group-prepend">
+<span class="input-group-text"><i class="far fa-clock"></i></span>
+</div>
+<input type="text" class="form-control float-right" id="reservationtime">
+</div>
+</div>
+</div>
+<!-- checkbox ประเภทกิจกรรม -->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label>ประเภทกิจกรรม</label>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="even1" value="option1">
+<label for="even1" class="custom-control-label"> สัมมนา </label>
+</div>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="even2" >
+<label for="even2" class="custom-control-label"> อบรม </label>
+</div>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="even3" >
+<label for="even3" class="custom-control-label"> ประชุม </label>
+</div>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="even3" >
+<label for="even3" class="custom-control-label"> บรรยาย </label>
+</div>
+</div>
+</div>
+<!-- radio รูปแบบห้องประชุม-->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label>รูปแบบจัดโต๊ะห้องประชุม</label>
+<div class="custom-control custom-radio">
+<input class="custom-control-input" type="radio" id="customRadio1" name="customRadio">
+<label for="customRadio1" class="custom-control-label"> 1 </label>
+</div>
+<div class="custom-control custom-radio">
+<input class="custom-control-input" type="radio" id="customRadio2" name="customRadio" >
+<label for="customRadio2" class="custom-control-label"> 2 </label>
+</div>
+<div class="custom-control custom-radio">
+<input class="custom-control-input" type="radio" id="customRadio3" name="customRadio">
+<label for="customRadio3" class="custom-control-label"> 3 </label>
+</div>
+</div>
+</div>
+
+<!-- checkbox อุปกรณ์/device -->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<label>อุปกรณ์</label>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="device1" value="option1">
+<label for="device1" class="custom-control-label"> 1 </label>
+</div>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="device2" >
+<label for="device2" class="custom-control-label"> 2 </label>
+</div>
+<div class="custom-control custom-checkbox">
+<input class="custom-control-input" type="checkbox" id="device3" >
+<label for="device3" class="custom-control-label"> 3 </label>
+</div>
+</div>
+</div>
+<!-- button ตกลง/ยกเลิก -->
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="exampleCheck1">
+<label class="form-check-label" for="exampleCheck1">ยืนยันการจอง</label>
+</div>
+<button type="submit" class="btn btn-primary"> ตกลง </button>
+<button type="submit" class="btn btn-default "> ยกเลิกการจอง </button>
+</div>
 
 </div>
 </div>
 </div>
 </div>
+</div>
+</div>
+</div>
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::resource('patterns','PatternController');
 
 //ฟอร์มกรอกข้อมูล
 Route::get('/form','FormController@index')->name('form');
+
+Route::resource('bookings','BookingController');
